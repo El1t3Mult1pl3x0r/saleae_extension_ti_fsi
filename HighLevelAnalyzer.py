d4d24a4809bec43069782d3cc023e442caccf6cf
El1t3Mult1pl3x0r <55260508+El1t3Mult1pl3x0r@users.noreply.github.com>
2026-02-15 19:29:40 +0100

    Implemented first version

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -1,41 +1,273 @@
-# High Level Analyzer
+# High Level Analyzer for TI FSI protocol
 # For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
 
-from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
+# ruff: noqa: T201, PLR2004
+
+from dataclasses import dataclass
+from enum import IntEnum, StrEnum, auto
+
+from saleae.analyzers import AnalyzerFrame, HighLevelAnalyzer, NumberSetting
+from saleae.data import SaleaeTime
+
+
+class FsiState(StrEnum):
+    IDLE = auto()
+    PREAMBLE = auto()
+    SOF = auto()
+    FRAMETYPE = auto()
+    USERDATA = auto()
+    DATA = auto()
+    CRC = auto()
+    FRAMETAG = auto()
+    EOF = auto()
+    POSTAMBLE = auto()
+
+
+class FsiFrameType(IntEnum):
+    PING = 0b0000
+    ERROR = 0b1111
+    DATA_1_WORD = 0b0100
+    DATA_2_WORD = 0b0101
+    DATA_4_WORD = 0b0110
+    DATA_6_WORD = 0b0111
+    DATA_N_WORD = 0b0011
+
+
+@dataclass
+class FsiFrame:
+    start_time: SaleaeTime | None = None
+    end_time: SaleaeTime | None = None
+    preamble: int = 0b1111
+    sof: int = 0b1001
+    frame_type: FsiFrameType | None = None
+    user_data: int | None = None
+    data_len: int = 0
+    data: tuple[int, ...] | None = None
+    crc: int | None = None
+    frame_tag: int | None = None
+    eof: int = 0b0110
+    postamble: int = 0b1111
 
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
 class Hla(HighLevelAnalyzer):
-    # List of settings that a user can set for this High Level Analyzer.
-    my_string_setting = StringSetting()
-    my_number_setting = NumberSetting(min_value=0, max_value=100)
-    my_choices_setting = ChoicesSetting(choices=('A', 'B'))
+    # Add user configuration
+    config_amnt_data_lines = NumberSetting(
+        label="Amount of data lines used.",
+        min_value=1,
+        max_value=2,
+    )
+    config_data_len = NumberSetting(
+        label='Data length in bytes when using "DATA_N_WORD" frame type.',
+        min_value=2,
+        max_value=32,
+    )
 
-    # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
-    result_types = {
-        'mytype': {
-            'format': 'Output type: {{type}}, Input type: {{data.input_type}}'
-        }
+    # A list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
+    result_types = {  # noqa: RUF012
+        "ti_fsi_frame": {
+            "format": "FSI | "
+            "Frame Type: {frame_type} | "
+            "User Data: {user_data} | "
+            "Data: {data} | "
+            "CRC: {crc} | "
+            "Frame Tag: {frame_tag}",
+        },
     }
 
-    def __init__(self):
-        '''
-        Initialize HLA.
+    def __init__(self) -> None:
+        super().__init__()
+        self.state: FsiState | None = FsiState.IDLE
+        self.fsi_frame = FsiFrame(
+            data_len=self.config_data_len if self.config_data_len is not None else 0,  # type: ignore[arg-type]
+        )  # type ignore[call-overload]
+        self.fb: list[int] = []  # Init frame buffer to store frames for later
 
-        Settings can be accessed using the same name used above.
-        '''
+        # Process amount of data lines setting
+        self.amnt_data_lines = 1
+        if self.config_amnt_data_lines is not None and (
+            self.config_amnt_data_lines < 1 or self.config_amnt_data_lines > 2  # type: ignore[operator]
+        ):
+            msg = (
+                '[TI FSI] The "Amount of data lines used." setting should be set to "1" or "2", '
+                "currently set to {self.config_amnt_data_lines}."
+            )
+            raise ValueError(msg)
+        if self.config_amnt_data_lines is not None and self.config_amnt_data_lines == 2:
+            self.amnt_data_lines = 2
 
-        print("Settings:", self.my_string_setting,
-              self.my_number_setting, self.my_choices_setting)
+    def decode(self, frame: AnalyzerFrame) -> AnalyzerFrame | None:  # noqa: C901, PLR0912, PLR0915
+        """Decode an FSI frame as described in section 12.4.5.4.4 of TI AM243x Technical Reference Manual (SPRUIM2I)."""
+        # First check correct frame type and get the bits from the frame
+        if frame.type != "data" or not isinstance(frame.data["data"], int):
+            msg = "[TI FSI] Analyzer only works with the 'Simple Parallel' input analyzer!"
+            raise NotImplementedError(msg)
 
-    def decode(self, frame: AnalyzerFrame):
-        '''
-        Process a frame from the input analyzer, and optionally return a single `AnalyzerFrame` or a list of `AnalyzerFrame`s.
+        bits = frame.data["data"]
 
-        The type and data values in `frame` will depend on the input analyzer.
-        '''
+        # Add bits to the frame buffer, how multi-lane data is handled depends on the state
+        if self.amnt_data_lines == 2 and self.state in (FsiState.USERDATA, FsiState.DATA, FsiState.CRC):
+            # userdata, data and crc can support two data lines.
+            # The data bits are alternated between the data lines, with the msb first on the first data line.
+            if bits == 0:
+                self.fb.append(0)
+                self.fb.append(0)
+            elif bits == 1:
+                self.fb.append(1)
+                self.fb.append(0)
+            if bits == 2:
+                self.fb.append(0)
+                self.fb.append(1)
+            elif bits == 3:
+                self.fb.append(1)
+                self.fb.append(1)
+            else:
+                msg = f"Data value is out-of-range, should be between 0-3, got {bits}. "
+                "Are the correct amount of data lines used in the analyzer settings?"
+                raise ValueError(msg)
+        else:
+            self.fb.append(bits & 1)
 
-        # Return the data frame itself
-        return AnalyzerFrame('mytype', frame.start_time, frame.end_time, {
-            'input_type': frame.type
-        })
+        # Process statemachine
+        while True:
+            match self.state:
+                case FsiState.IDLE:
+                    self.fsi_frame = FsiFrame(data_len=self.config_data_len if self.config_data_len is not None else 0)  # type: ignore[arg-type]
+                    self.fb.clear()
+                    self.state = FsiState.PREAMBLE
+                    continue
+                case FsiState.PREAMBLE:
+                    if len(self.fb) >= 4 and self.fb[-4:] == [1, 1, 1, 1]:
+                        self.fsi_frame.start_time = frame.start_time
+                        self.fsi_frame.preamble = 0b1111
+                        self.fb.clear()
+                        self.state = FsiState.SOF
+                    break
+                case FsiState.SOF:
+                    if len(self.fb) < 4:
+                        break
+                    if self.fb == [1, 0, 0, 1]:
+                        self.fsi_frame.sof = 0b1001
+                        self.fb.clear()
+                        self.state = FsiState.FRAMETYPE
+                    else:  # Error
+                        self.state = None
+                        continue
+                    break
+                case FsiState.FRAMETYPE:
+                    if len(self.fb) < 4:
+                        break
+                    try:
+                        fsi_frame_type = FsiFrameType(self._bitlist_to_byte(self.fb))
+                    except ValueError:  # Error
+                        print(f"[TI FSI] Invalid frame type: {self._bitlist_to_byte(self.fb)}")
+                        self.state = None
+                        continue
+                    self.fsi_frame.frame_type = fsi_frame_type
+                    self.fb.clear()
+                    if fsi_frame_type in (FsiFrameType.PING, FsiFrameType.ERROR):
+                        self.state = FsiState.FRAMETAG
+                    else:
+                        if fsi_frame_type == FsiFrameType.DATA_1_WORD:
+                            self.fsi_frame.data_len = 2
+                        elif fsi_frame_type == FsiFrameType.DATA_2_WORD:
+                            self.fsi_frame.data_len = 4
+                        elif fsi_frame_type == FsiFrameType.DATA_4_WORD:
+                            self.fsi_frame.data_len = 8
+                        elif fsi_frame_type == FsiFrameType.DATA_6_WORD:
+                            self.fsi_frame.data_len = 12
+                        self.state = FsiState.USERDATA
+                    break
+                case FsiState.USERDATA:
+                    if len(self.fb) < 8:
+                        break
+                    self.fsi_frame.user_data = self._bitlist_to_byte(self.fb)
+                    self.fb.clear()
+                    self.state = FsiState.DATA
+                    break
+                case FsiState.DATA:
+                    if len(self.fb) < (self.fsi_frame.data_len * 8):
+                        break
+                    self.fsi_frame.data = tuple(self._bitlist_to_bytelist(self.fb))
+                    self.fb.clear()
+                    self.state = FsiState.CRC
+                    break
+                case FsiState.CRC:
+                    if len(self.fb) < 8:
+                        break
+                    self.fsi_frame.crc = self._bitlist_to_byte(self.fb)
+                    self.fb.clear()
+                    self.state = FsiState.FRAMETAG
+                    break
+                case FsiState.FRAMETAG:
+                    if len(self.fb) < 4:
+                        break
+                    self.fsi_frame.frame_tag = self._bitlist_to_byte(self.fb)
+                    self.fb.clear()
+                    self.state = FsiState.EOF
+                    break
+                case FsiState.EOF:
+                    if len(self.fb) < 4:
+                        break
+                    if self.fb == [0, 1, 1, 0]:
+                        self.fsi_frame.eof = 0b0110
+                        self.fb.clear()
+                        self.state = FsiState.POSTAMBLE
+                    else:  # Error
+                        self.state = None
+                        continue
+                    break
+                case FsiState.POSTAMBLE:
+                    if len(self.fb) < 4:
+                        break
+                    if self.fb == [1, 1, 1, 1]:
+                        self.fsi_frame.end_time = frame.start_time
+                        self.fsi_frame.postamble = 0b1111
+                        self.fb.clear()
+                        self.state = FsiState.IDLE
+                        # Return analyzerframe
+                        return AnalyzerFrame(
+                            "ti_fsi_frame",
+                            self.fsi_frame.start_time,
+                            self.fsi_frame.end_time,
+                            {
+                                "frame_type": self.fsi_frame.frame_type.name if self.fsi_frame.frame_type else "None",
+                                "user_data": hex(self.fsi_frame.user_data) if self.fsi_frame.user_data else "None",
+                                "data": " ".join(hex(b) for b in self.fsi_frame.data)
+                                if self.fsi_frame.data
+                                else "None",
+                                "crc": hex(self.fsi_frame.crc) if self.fsi_frame.crc else "None",
+                                "frame_tag": hex(self.fsi_frame.frame_tag) if self.fsi_frame.frame_tag else "None",
+                            },
+                        )
+                    # Error
+                    self.state = None
+                    continue
+                case _:
+                    print("[TI FSI] State machine in invalid state, frame corrupt!")
+                    self.state = FsiState.IDLE
+                    continue
+
+        return None
+
+    def _bitlist_to_byte(self, bitlist: list[int]) -> int:
+        # Only works with max 8 bits
+        if len(bitlist) > 8:
+            msg = f"[TI FSI] _bitlist_to_byte only allows up to 8 bits, provided {len(bitlist)} bits!"
+            raise ValueError(msg)
+        byte_val = 0
+        for bit in bitlist:
+            byte_val = (byte_val << 1) | bit
+        return byte_val
+
+    def _bitlist_to_bytelist(self, bitlist: list[int]) -> list[int]:
+        bytelist = []
+        if len(bitlist) % 8:
+            # append 0 to get multiple of 8
+            bitlist.extend([0] * (8 - (len(bitlist) % 8)))
+        for i in range(0, len(bitlist), 8):
+            byte_val = 0
+            for bit in bitlist[i : i + 8]:
+                byte_val = (byte_val << 1) | bit
+            bytelist.append(byte_val)
+        return bytelist
--- README.md
+++ README.md
@@ -4,11 +4,21 @@
 
 The Texas Instruments Fast Serial Interface peripheral/protocol is available on many TI MCUs and MPUs.
 
-This high level analyzer for Saleae Logic decodes the messages from this protocol.
+This high level analyzer decodes the messages from this protocol.
+
+## How to use
+
+1. Create a 'Simple Parallel' analyzer with the clock, d0 and optionally d1 (if used). Clock state should be 'Dual edge'.
+1. Create a 'TI FSI' analyzer:
+    1. Select the just created Simple Parallel analyzer as the 'Input analyzer'.
+    1. Set the 'Amount of data lines used.' to '1' if only D0 data line is used, if also D1 data line is used, set the value to '2'.
+    1. When using the 'DATA_N_WORD' frame type in your FSI implementation, set the correct data length in **bytes** (e.g. 16 words = 32 bytes). Otherwise, leave it at the default.
+
+## TODO
+
+* Remove getting started section.
 
 ## Getting started
-
-*TODO remove*
 
 1. Build your extension by updating the Python files for your needs
 2. Create a public Github repo and push your code
